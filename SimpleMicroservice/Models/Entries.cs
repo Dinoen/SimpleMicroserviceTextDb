26feec4d335ba6caf8da846402ed040730d4c9bb
Dinoen <rex1994@gmail.com>
2024-07-27 23:21:49 +0200

    put summary in the entries model

--- SimpleMicroservice/Models/Entries.cs
+++ SimpleMicroservice/Models/Entries.cs
@@ -1,8 +1,17 @@
 ﻿namespace SimpleMicroservice.Models
 {
+    /// <summary>
+    /// Represents an entry in the system that can be written and read via the api.
+    /// </summary>
     public class Entry
     {
+        /// <summary>
+        /// Gets or sets Entry ID.
+        /// </summary>
         public int Id { get; set; }
+        /// <summary>
+        /// Gets or sets Entry data.
+        /// </summary>
         public string Data { get; set; }
     }
 
